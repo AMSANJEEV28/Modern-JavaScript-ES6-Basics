c88aa1085767eac46227a81e71e4a913de59c01b
amsanjeev28 <amsanjeev28@gmail.com>
2023-08-06 09:03:00 +0000

    Strings and Interpolation

--- es6/index.js
+++ es6/index.js
@@ -121,25 +121,24 @@ prepareChai(ingredients);
 
 // TASK 5: Exercise: Refactor the code to use the ES6 String utility methods
 
-// const country = "Bulgaria";
-// const city = "Sofia";
+const country = "Bulgaria";
+const city = "Sofia";
 
-// if (country.indexOf("Bulg") > - 1) {
-//     console.log("The country starts with Bulg");
-// }
+if (country.startsWith("Bulg")) {
+    console.log("The country starts with Bulg");
+}
 
-// if (city.indexOf("So") === 0) {
-//     console.log("The name starts with So");
-// }
+if (city.startsWith("So")) {
+    console.log("The name starts with So");
+}
 
-// if (city.lastIndexOf("a") === city.length - 1) {
-//     console.log("The name ends with a");
-// }
+if (city.endsWith("a")) {
+    console.log("The name ends with a");
+}
 
-// console.log(
-//     "The capital of " + country +
-//     " is the city of " + city
-// );
+console.log(
+    'The capital of ${country} is the city of ${city}'
+);
 
 
 
